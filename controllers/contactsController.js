249ca147cc39ac1447351e54ac677507b1ad4614
Prashant Chauhan <prashant.chauhan@globalvoxinc.com>
2023-05-09 15:21:07 +0530

    express async handler added into the app to handle async function error

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,42 +1,44 @@
+const asyncHandler = require("express-async-handler");
+
 // @des Get all contacts
 // @route GET /api/contacts
 // @access public
-const getContactsController = (req, res) => {
+const getContactsController = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 // @des Create contact
 // @route POST /api/contacts
 // access public
-const createContactController = (req, res) => {
+const createContactController = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are mendatory");
   }
   res.status(200).json({ message: "Create contact" });
-};
+});
 
 // @des Get contact
 // @route GET /api/contacts/:id
 // @access public
-const getContactController = (req, res) => {
+const getContactController = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
 // @des Update contact
 // @route PUT /api/contacts/:id
 // @access public
-const updateContactController = (req, res) => {
+const updateContactController = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact ${req.params.id}` });
-};
+});
 
 // @des  Delete contact
 // @route DELETE /api/contacts/:id
 // @access public
-const deleteContactController = (req, res) => {
+const deleteContactController = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContactsController,
