8a59858890617ae7897ae3da4ad758b123f06460
Prashant Chauhan <prashant.chauhan@globalvoxinc.com>
2023-05-09 13:48:36 +0530

    status code changed to 400

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -11,7 +11,7 @@ const getContactsController = (req, res) => {
 const createContactController = (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
-    res.status(401);
+    res.status(400);
     throw new Error("All fields are mendatory");
   }
   res.status(200).json({ message: "Create contact" });
