9f1b683e0aa083a85dbf133906d08041cb2910fb
Prashant Chauhan <prashant.chauhan@globalvoxinc.com>
2023-05-09 10:47:01 +0530

    express router and contact crud route setup

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 dotenv.config();
 
+// contact route
+app.use("/api/contact", require("./routes/contactRoutes"));
+
+app.get("/", (req, res) =>
+  res.status(200).json({ message: "root of the apis" })
+);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
