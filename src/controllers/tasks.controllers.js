ccdeaedea0fe1409c1100ac556f9c343fb0cdc42
Prashant Chauhan <pnc439@gmail.com>
2023-10-29 02:06:23 +0530

    [Feature] Added asynce handler function into the app

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,34 +1,35 @@
-const Task = require("src/models/task.models");
+const Task = require("src/models/Task.models");
+const asyncHandler = require("express-async-handler");
 
-const getTasks = (req, res) => {
+const getTasks = asyncHandler((req, res) => {
   res.status(200).send("Get all tasks");
-};
-const addTask = async (req, res) => {
+});
+const addTask = asyncHandler(async (req, res) => {
   const addedTask = await Task.create(req.body);
   res.status(201).json(addedTask);
-};
-const getTask = (req, res) => {
+});
+const getTask = asyncHandler((req, res) => {
   const { taskId } = req.params;
   res.status(200).json({
     message: `Get task details of task ${taskId}`,
     taskId,
   });
-};
-const updateTask = (req, res) => {
+});
+const updateTask = asyncHandler((req, res) => {
   const { taskId } = req.params;
   res.status(200).json({
     message: `Get task details of task ${taskId}`,
     taskId,
     ...req.body,
   });
-};
-const deleteTask = (req, res) => {
+});
+const deleteTask = asyncHandler((req, res) => {
   const { taskId } = req.params;
   res.status(200).json({
     message: `Deleted task ${taskId}`,
     taskId,
   });
-};
+});
 
 module.exports = {
   getTasks,
